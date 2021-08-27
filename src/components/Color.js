e15f9270f0c9514d436647c99fd5c21de5f3e10d
Melnik Egor, Xubuntu <melnikeg@ukr.net>
2021-08-27 14:26:31 +0300

    Work on styles

--- src/components/Color.js
+++ src/components/Color.js
@@ -3,13 +3,12 @@ import s from "./Color.module.css";
 
 const Color = ({ title, color, rating = 0, onRemove = f => f, onRate = f => f }) =>
     <div className={s.box}>
-        <div className={s.box3}>
-            <h1>{title}</h1>
+        <div className={s.box2}>
+            {title}
         </div>
         <div>
             <button onClick={onRemove}>X</button>
         </div>
-
         <div className={s.box1}
             style={{ backgroundColor: color }}>
         </div>
--- src/components/Star.js
+++ src/components/Star.js
@@ -1,7 +1,7 @@
 import style from './Star.module.css';
 
 const Star = ({ selected=false, onClick=f=>f}) =>
-<div className={(selected) ? (style.star + ' ' + style.selected) : (style.star)}
+<div className={(selected) ? (style.star + ' ' + style.starSelected) : (style.star)}
     onClick={onClick}>
 </div>
 
